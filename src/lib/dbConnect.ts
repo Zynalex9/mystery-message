a6b59c88a040b4be268f8362d8158aa2a1754e64
Zynalex9 <alzn952@gmail.com>
2024-09-19 18:06:44 +0500

    DB Connection

---
+++ src/lib/dbConnect.ts
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+type ConnectionObj = {
+  isConnected?: number;
+};
+
+const connection: ConnectionObj = {};
+export async function dbConnect(): Promise<void> {
+    console.log("dbConnect called");
+  if (connection.isConnected) {
+    console.log("DB already Connected");
+    return;
+  }
+  try {
+    const db = await mongoose.connect(process.env.MONGODB_URI!);
+    // console.log(db)
+    connection.isConnected = db.connections[0].readyState
+    console.log('DB Connected Sucessfully')
+  } catch (error: any) {
+    console.log(error.message);
+  }
+}
