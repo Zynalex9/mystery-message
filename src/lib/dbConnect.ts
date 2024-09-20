070bf7191391c2b4dd21d8a68178890951008368
Zynalex9 <alzn952@gmail.com>
2024-09-20 10:33:53 +0500

    New changes

--- src/lib/dbConnect.ts
+++ src/lib/dbConnect.ts
@@ -5,17 +5,19 @@ type ConnectionObj = {
 
 const connection: ConnectionObj = {};
 export async function dbConnect(): Promise<void> {
-    console.log("dbConnect called");
+  console.log("dbConnect called");
   if (connection.isConnected) {
     console.log("DB already Connected");
     return;
   }
   try {
-    const db = await mongoose.connect(process.env.MONGODB_URI!);
+    const db = await mongoose.connect(process.env.MONGODB_URI! || '');
     // console.log(db)
-    connection.isConnected = db.connections[0].readyState
-    console.log('DB Connected Sucessfully')
+    connection.isConnected = db.connections[0].readyState;
+    console.log("DB Connected Sucessfully");
   } catch (error: any) {
+    console.log("DB Not Connected");
     console.log(error.message);
+    process.exit();
   }
 }
